149b94dfdc6ac679a06f62aa07f42329e8f71a8b
thuanquang1998 <thuanquang2009@gmail.com>
2022-07-13 13:46:55 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from '@monorepo/common';
+
+function App() {
+  return (
+    <div className="App">
+        <Button textColor="red">Hello</Button>
+    </div>
+  );
+}
+
+export default App;
